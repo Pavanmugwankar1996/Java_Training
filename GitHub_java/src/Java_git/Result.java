e542a1c9f29919b369283b3d0856c853474d1afe
Pavanmugwankar1996 <pavan.mugwankar@gmail.com>
2021-08-27 18:48:21 +0530

    Commit

---
+++ GitHub_java/src/Java_git/Result.java
@@ -0,0 +1,12 @@
+package Java_git;
+
+public class Result {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to GitHub");
+
+	}
+
+}
